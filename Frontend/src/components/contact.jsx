430c125cadda208cf0733573d949774604aeee85
deepakgupta75 <125240693+deepakgupta75@users.noreply.github.com>
2024-09-11 09:51:13 +0530

    Update contact.jsx

--- Frontend/src/components/contact.jsx
+++ Frontend/src/components/contact.jsx
@@ -39,7 +39,7 @@ const Contact = () => {
     } else {
       try {
         // Send form data to API
-        const response = await fetch("http://localhost:5000/api/contact", {
+        const response = await fetch("https://enhanced-contact-us-system-11.onrender.com/api/contact", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
